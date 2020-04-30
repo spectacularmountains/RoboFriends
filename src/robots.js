42179168f10b8b31e10d061ccc8e60b3823f9433
spectacularmountains <cornelius.rott@gmail.com>
2020-04-30 15:54:02 -0600

    Set up basic structure of RoboFriends app

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1, 
+        name: "Wilhelm", 
+        username: "wiro",
+        email: "wiro@wiro.com",
+    }, 
+    {
+        id: 2, 
+        name: "Ingrid", 
+        username: "inro",
+        email: "inro@inro.com",
+    },
+    {
+        id: 3, 
+        name: "Angelika", 
+        username: "geli",
+        email: "geli@geli.com",
+    },
+    {
+        id: 4, 
+        name: "Anna", 
+        username: "anna",
+        email: "anna@anna.com",
+    },
+    {
+        id: 5, 
+        name: "Clara", 
+        username: "clara",
+        email: "clara@clara.com",
+    }
+]
