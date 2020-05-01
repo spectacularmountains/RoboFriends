ff6a808a6af2910934c4f68268f597dd6e518056
spectacularmountains <cornelius.rott@gmail.com>
2020-05-01 11:09:03 -0600

    Updates

---
+++ precache-manifest.38d1764c40ea6f03ae4c2113ead24de1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f0c7f42145313dc097d1215df701f8",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/RoboFriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "02e8753635d2e02d653d",
+    "url": "/RoboFriends/static/css/main.64503b7f.chunk.css"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/RoboFriends/static/js/2.479e6b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.479e6b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e8753635d2e02d653d",
+    "url": "/RoboFriends/static/js/main.50465560.chunk.js"
+  },
+  {
+    "revision": "26cbbaac60d1b32545c2",
+    "url": "/RoboFriends/static/js/runtime-main.ac2dadd7.js"
+  },
+  {
+    "revision": "762eeebef282012382c57dce1a6e0d16",
+    "url": "/RoboFriends/static/media/Aeroshodic.762eeebe.ttf"
+  }
+]);
